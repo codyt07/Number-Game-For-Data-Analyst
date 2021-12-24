ee35a95060b119f69eb00e033401162f5c028e99
codyt07 <89680528+codyt07@users.noreply.github.com>
2021-12-23 20:55:03 -0600

    PEP 8 verified and added ReadMe

---
+++ README.txt
@@ -0,0 +1,14 @@
+Welcome to version 2 of my Python number guessing game! 
+
+I created the original for the Python Tech Degree at Team Treehouse. 
+Since then, I have earned my certificate and started their Data Analysis path. 
+This program is very similar. The user has to guess a randomly generated number 
+between 1 through 100. Their goal is to guess the number in the least
+ amount of guesses. 
+
+After the user correctly guesses the number, this program will 
+show statistics based on how the user performs. It stores the 
+scores inside of a score array, and it displays to the user the 
+following: Best Score, Mean, Median, and Mode of their scores. 
+
+Thank you for viewing my project! 
--- guessing_game.py
+++ guessing_game.py
@@ -1,12 +1,13 @@
-#Number guessing game for Data Analysis Project
+# Number guessing game for Data Analysis Project
 import random
 from statistics import mode, median
 
-#Scorecard for mean, median, mode
+# Scorecard for mean, median, mode, and best score
 SCORE = []
 
+
 def main_game():
-    winning_number = random.randint(1,100)
+    winning_number = random.randint(1, 100)
     if len(SCORE) > 0:
         best_score = min(SCORE)
         print(f'\n** You best score so far is {best_score}!')
@@ -23,7 +24,7 @@ def main_game():
                 print("You did not enter a number between 1 - 100")
             elif int_guess > winning_number:
                     print('The number is lower!')
-                    guesses += 1 
+                    guesses += 1
             elif int_guess < winning_number:
                     print('The number is higher!')
                     guesses += 1
@@ -31,27 +32,27 @@ def main_game():
                 SCORE.append(guesses)
                 print(f'''You guessed the correct number {winning_number}!
                 \nIt took you {guesses} Tries!''')
-                new_game() 
-                break       
+                new_game()
+                break
         except ValueError:
             print('A valid number was not entered!')
 
+
 def new_game():
-    #mean
+    # Mean
     total = sum(SCORE)
     divide = len(SCORE)
     mean = total / divide
-    #median
+    # Median
     game_median = median(SCORE)
-    #mode
+    # Mode
     game_mode = mode(SCORE)
-    #best score
+    # Best score
     best_score = min(SCORE)
     print(f'''\nThe Mean/Average of your scores is {mean}
     \rThe Median of your scores is {game_median}
     \rThe Mode of your scores is {game_mode}
     \rYour best score is {best_score}''')
-    
     while True:
         new_game = input('''\nDo you want to play again?
             \rEnter A to play again
@@ -68,3 +69,4 @@ def new_game():
 
 if __name__ == '__main__':
     main_game()
+
